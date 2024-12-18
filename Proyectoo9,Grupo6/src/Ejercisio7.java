f03d098571ec4e6947bad3605e411287b26fc42e
Juan2306 <juanquizhpe7@gmail.com>
2024-12-18 14:34:56 -0500

    grupal realizado

--- Proyectoo9,Grupo6/src/Ejercisio7.java
+++ Proyectoo9,Grupo6/src/Ejercisio7.java
@@ -2,6 +2,7 @@
 import java.util.Random;
 /**
  * El primer ciclo paralelo C, cuenta con 28 estudiantes, de los cuales al finalizar el ciclo, la Dirección de la carrera de Computación a solicitado las siguientes estadísticas en función a los promedios obtenidos del ciclo por estudiantes (use 1 arreglo, no matrices, y para el promedio por estudiante, no ingrese el valor, si se debe autogenerar).
+ * @Juan Quizhpe
  */
 public class Ejercisio7 {
     public static void main(String[] args) {
@@ -61,39 +62,67 @@ public class Ejercisio7 {
     }
 }
 /***
- * Promedio del ciclo: 4.612211679315018
+ * 1.1597400919045653 , 
+0.7201429641909407 , 
+8.786714284301617 , 
+8.695522615171694 , 
+6.391909003925933 , 
+4.089467134194207 , 
+8.397165837324406 , 
+4.539888720418418 , 
+8.613317354906465 , 
+1.964277378505015 , 
+0.4911165400082862 , 
+2.287556593163884 , 
+6.4445546579172985 , 
+4.013417695478067 , 
+3.487202945808403 , 
+6.947714653622513 , 
+7.539447049484379 , 
+9.78883862648918 , 
+6.68505360358962 , 
+3.6200527534601967 , 
+6.576682483744087 , 
+3.285381515158176 , 
+3.9652725985483728 , 
+4.823028759546453 , 
+6.967421784860219 , 
+0.5937573105531024 , 
+0.8539929399535218 , 
+4.407915758708338 , 
+Promedio del ciclo: 4.86201977339062
 Estudiantes con calificación por encima del promedio:
-Estudiante 1: 5.0046053649741165
-Estudiante 2: 7.479823874986419
-Estudiante 6: 4.620357790077623
-Estudiante 7: 7.4001988723741725
-Estudiante 8: 8.592561055444826
-Estudiante 9: 6.725721989055989
-Estudiante 12: 8.205986489994558
-Estudiante 13: 9.048229233869876
-Estudiante 17: 5.8876585422270145
-Estudiante 19: 8.143651715425285
-Estudiante 21: 5.951366726890962
-Estudiante 23: 6.188559949685294
-Estudiante 28: 7.88609503383643
+Estudiante 3: 8.786714284301617
+Estudiante 4: 8.695522615171694
+Estudiante 5: 6.391909003925933
+Estudiante 7: 8.397165837324406
+Estudiante 9: 8.613317354906465
+Estudiante 13: 6.4445546579172985
+Estudiante 16: 6.947714653622513
+Estudiante 17: 7.539447049484379
+Estudiante 18: 9.78883862648918
+Estudiante 19: 6.68505360358962
+Estudiante 21: 6.576682483744087
+Estudiante 25: 6.967421784860219
 
 Estudiantes con calificación por debajo del promedio:
-Estudiante 3: 2.0078374240665475
-Estudiante 4: 4.5198050889375265
-Estudiante 5: 2.0001928301920966
-Estudiante 10: 2.966693075025495
-Estudiante 11: 2.460184814699776
-Estudiante 14: 2.8344900335434975
-Estudiante 15: 0.4259288333039146
-Estudiante 16: 3.0714600504216136
-Estudiante 18: 0.5984914444032985
-Estudiante 20: 4.4471512187655895
-Estudiante 22: 3.022071565974196
-Estudiante 24: 4.211454567240017
-Estudiante 25: 2.3111977809225515
-Estudiante 26: 2.0341043580095706
-Estudiante 27: 1.0960472964722168
+Estudiante 1: 1.1597400919045653
+Estudiante 2: 0.7201429641909407
+Estudiante 6: 4.089467134194207
+Estudiante 8: 4.539888720418418
+Estudiante 10: 1.964277378505015
+Estudiante 11: 0.4911165400082862
+Estudiante 12: 2.287556593163884
+Estudiante 14: 4.013417695478067
+Estudiante 15: 3.487202945808403
+Estudiante 20: 3.6200527534601967
+Estudiante 22: 3.285381515158176
+Estudiante 23: 3.9652725985483728
+Estudiante 24: 4.823028759546453
+Estudiante 26: 0.5937573105531024
+Estudiante 27: 0.8539929399535218
+Estudiante 28: 4.407915758708338
 
-Estudiante con la mejor calificación: Estudiante 13 con calificación: 9.048229233869876
-Estudiante con la peor calificación: Estudiante 15 con calificación: 0.4259288333039146
+Estudiante con la mejor calificación: Estudiante 18 con calificación: 9.78883862648918
+Estudiante con la peor calificación: Estudiante 11 con calificación: 0.4911165400082862
  */
